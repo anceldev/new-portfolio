9930b1e5968c7ba06fa1672cbf9c80bdf6e34509
Ancel Guarayo <aguarayo@gmail.com>
2024-06-05 17:22:04 +0200

    Tailwind added

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+  es: "Spanish",
+  en: "English"
+}
+
+export const defaultLang = 'es';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.projects': 'Projects',
+    'nav.contact': 'Contact'
+  },
+  es: {
+    'nav.home': 'Inicio',
+    'nav.about': 'Sobre mi',
+    'nav.projects': 'Proyectos',
+    'nav.contact': 'Contacto',
+  },
+} as const;
