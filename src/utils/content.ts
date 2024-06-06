fabb18cb479ee950a1ba07fc44f2b6c2c7910757
Ancel Guarayo <aguarayo@gmail.com>
2024-06-06 16:04:41 +0200

    Work and Header components added

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -5,7 +5,44 @@ export const content = {
       title2: 'Developer',
       button: 'Projects',
       description: 'My goal is to write maintainable, clean and undestandable code to process development was enjoyable'
-    }
+    },
+    about: {
+      title: '.../About me...',
+      description: 'Hello! I\'m Ancel, I\'m a mobile developer. More than 2 years experience.',
+    },
+    work: {
+      title: 'Work',
+      experience: [
+        {
+          years: '2022 -',
+          months: '1 year 5 months',
+          company: 'ITHUB',
+          job: 'Frontend developer | React & Vue',
+        },
+        {
+          years: '2021 - 2022',
+          months: '8 months',
+          company: 'VK Development Lab',
+          job: 'Frontend developer | React',
+        },
+        {
+          years: '2020-2021',
+          months: '9 months',
+          company: 'SN Inc.',
+          job: 'Fullstack developer | Javascript & Python',
+        },
+        {
+          years: '2018 - 2020',
+          months: '1 year 11 months',
+          company: 'Business Up',
+          job: 'Fullstack developers | Javascript & Python',
+        }
+      ],
+      footer: {
+        title: 'Experiencia laboral',
+        duration: '4 años 9 meses'
+      }
+    },
   },
   es: {
     header: {
@@ -13,6 +50,43 @@ export const content = {
       title2: 'Móvil',
       button: 'Proyectos',
       description: `Mi objetivo es escribir código limpio, mantenible y entendible para el proceso de desarrollo.`
-    }
+    },
+    about: {
+      title: '.../Sobre mi...',
+      description: 'Hola! Me llamo Ancel, Soy desarrollador de aplicaciones móviles. Más de 2 años de experiencia.',
+    },
+    work: {
+      title: 'Experiencia',
+      experience: [
+        {
+          years: '2022 -',
+          months: '1 año 5 meses',
+          company: 'ITHUB',
+          job: 'Frontend developer | React & Vue',
+        },
+        {
+          years: '2021 - 2022',
+          months: '8 meses',
+          company: 'VK Development Lab',
+          job: 'Frontend developer | React',
+        },
+        {
+          years: '2020-2021',
+          months: '9 meses',
+          company: 'SN Inc.',
+          job: 'Fullstack developer | Javascript & Python',
+        },
+        {
+          years: '2018 - 2020',
+          months: '1 año 11 meses',
+          company: 'Business Up',
+          job: 'Fullstack developers | Javascript & Python',
+        }
+      ],
+      footer: {
+        title: 'Work Experience',
+        duration: '4 years 9 months'
+      },
+    },
   }
 }
