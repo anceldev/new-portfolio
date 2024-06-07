ef57b78bfa2d7ee14a95610f2b4630fdacd8ee96
Ancel Guarayo <aguarayo@gmail.com>
2024-06-07 21:24:28 +0200

    projects added

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -161,6 +161,29 @@ export const content = {
           linkProject: 'https://github.com/anceldev/ExpenseTracker',
         },
         {
+          title: 'Movix',
+          technologies: [
+            'Swift',
+            'SwiftUI',
+            'Firebase',
+          ],
+          description: 'Movix es una aplicación de streaming en la que podrás ver tus películas y series favoritas. Ademas, podrás compartir esa experiencia con la persona que quieras.',
+          imgUrl: 'project2.png',
+          linkProject: 'https://github.com/anceldev/Movix',
+        },
+        {
+          title: 'La Roca Manager',
+          technologies: [
+            'Typescript',
+            'React',
+            'Appwrite',
+            'Tailwind CSS',
+          ],
+          description: 'La Roca Manager es una aplicación web utilizada para la gestión y control de usuarios dentro de una organización. A través de la misma se puede controlar las formaciones que realizan los usuarios, llevar un historial de sus acciones y almacenar sus datos.',
+          imgUrl: 'project3.png',
+          linkProject: 'https://github.com/anceldev/larocacyl-manager',
+        },
+        {
           title: 'Kana Master',
           technologies: [
             'Typescript',
