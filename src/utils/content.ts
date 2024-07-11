6fc89132427663175c643ada1848068a6e9edbcb
Ancel Guarayo <aguarayo@gmail.com>
2024-07-11 19:24:56 +0200

    images url

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -59,7 +59,7 @@ export const content = {
             'Firebase',
           ],
           description: 'Expense Tracker is an application that will help you take better control of your finances. With it you can manage all your expenses in a more detailed way by adding categories and even attaching their receipt.',
-          imgUrl: 'project1.png',
+          imgUrl: '/public/project1.png',
           linkProject: 'https://github.com/anceldev/ExpenseTracker',
         },
         {
@@ -72,7 +72,7 @@ export const content = {
             'iOS',
           ],
           description: 'Kana Master is an iOS application designed for learning Katakana and Hiragana. It includes various tests and practical exercises that help in learning and memorizing Japanese characters.\n\nThe app also offers audio training for correct pronunciation and demonstrates how to propery draw each character.',
-          imgUrl: 'project2.png',
+          imgUrl: '/public/project2.png',
           linkProject: '',
         },
         {
@@ -86,7 +86,7 @@ export const content = {
             'gocron',
           ],
           description: 'Hey, anime fans! Struggling to track new episodes? Anime Alert Bot is your solution. Get auto-notifications for new episode releases!\n\nHot It Works:\n* Send the bot your anime linek.\n* Get release schdules instantly. Skip searching the web or social media.\n* Every 30 mins, the bot checks for new episodes using a cron schedule. Be the first to know about voice or subtitle updates!.\n\nConnect now and neves miss an episode!',
-          imgUrl: 'project3.png',
+          imgUrl: '/project/project3.png',
           linkProject: '',
         },
       ],
@@ -157,7 +157,7 @@ export const content = {
             'Firebase',
           ],
           description: 'Expense Tracker es una aplicación que te ayudará a llevar un control más ordenador acerca de tus finanzas. Con ella podrás gestionar todos tus gastos de una manera más detallada añadiendo categorías e incluso adjuntando el recibo de los mismos.',
-          imgUrl: 'project1.png',
+          imgUrl: '/public/project1.png',
           linkProject: 'https://github.com/anceldev/ExpenseTracker',
         },
         {
@@ -168,7 +168,7 @@ export const content = {
             'Firebase',
           ],
           description: 'Movix es una aplicación de streaming en la que podrás ver tus películas y series favoritas. Ademas, podrás compartir esa experiencia con la persona que quieras.',
-          imgUrl: 'project2.png',
+          imgUrl: '/public/project2.png',
           linkProject: 'https://github.com/anceldev/Movix',
         },
         {
@@ -180,7 +180,7 @@ export const content = {
             'Tailwind CSS',
           ],
           description: 'La Roca Manager es una aplicación web utilizada para la gestión y control de usuarios dentro de una organización. A través de la misma se puede controlar las formaciones que realizan los usuarios, llevar un historial de sus acciones y almacenar sus datos.',
-          imgUrl: 'project3.png',
+          imgUrl: '/public/project3.png',
           linkProject: 'https://github.com/anceldev/larocacyl-manager',
         },
         {
@@ -193,7 +193,7 @@ export const content = {
             'iOS',
           ],
           description: 'Kana Master es una aplicación para iOS diseñada para aprender Katakana e Hiragana. Incluye varias pruebas y ejercicios prácticos que ayudan a aprender y memorizar los caracteres japoneses.\n\nLa aplicación también ofrece entrenamiento de audio para la pronunciación correcta y demuestra cómo dibujar correctamente cada carácter.',
-          imgUrl: 'project2.png',
+          imgUrl: '/public/project2.png',
           linkProject: '',
         },
         {
@@ -207,7 +207,7 @@ export const content = {
             'gocron',
           ],
           description: `¡Hola, fanáticos del anime! ¿Tienes problemas para seguir nuevos episodios? Anime Alert Bot es tu solución. ¡Recibe notificaciones automáticas para los lanzamientos de nuevos episodios!\n\nHot It Works:\n\n* Envía al robot tu línea de anime.\n\n* Obtén los horarios de lanzamiento al instante. Evite buscar en la web o en las redes sociales.\n* Cada 30 minutos, el bot busca nuevos episodios mediante una programación cron. ¡Sé el primero en enterarte de las actualizaciones de voz o subtítulos!.\n\n¡Conéctate ahora y nunca te perderás un episodio!`,
-          imgUrl: 'project3.png',
+          imgUrl: '/public/project3.png',
           linkProject: '',
         },
       ],
