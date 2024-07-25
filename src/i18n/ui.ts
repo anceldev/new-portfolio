ffe7f17f8f288ce93eb08b73c6f7e051aa6374d8
Ancel Guarayo <aguarayo@gmail.com>
2024-07-25 10:59:56 +0200

    Pre 'en' translation

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,12 +10,46 @@ export const ui = {
     'nav.home': 'Home',
     'nav.about': 'About',
     'nav.projects': 'Projects',
-    'nav.contact': 'Contact'
+    'nav.contact': 'Contact',
+    
+    'header.title1': 'iOS & Web',
+    'header.title2': 'Developer',
+    'header.description': 'My goal is to write maintainable, clean and undestandable code to process development was enjoyable',
+    
+    'about.title': 'About me',
+    'about.description': '<p class="text-lightGray text-xl font-light font-opensans lg:text-2xl lg:size-1/2">Hello! I\'m Ancel, I\'m a <span class="text-dirtyWhite italic">mobile developer</span> with more than <span class="text-dirtyWhite italic">2 years experience</span></p>',
+    
+    'work.title': 'Work',
+    'work.duration': '4 years 9 months',
+
+    'projects.title': 'Projects',
+
+    'contact.title': ' Contact',
+    'contact.mail': 'anceltests@gmail.com',
+    'contact.phone': '626 655 430',
+    'contact.cookies': 'Cookies Policy'
   },
   es: {
     'nav.home': 'Inicio',
     'nav.about': 'Sobre mi',
     'nav.projects': 'Proyectos',
     'nav.contact': 'Contacto',
+    
+    'header.title1': 'Desarrollador',
+    'header.title2': 'iOS & Web',
+    'header.description': 'Mi objetivo es escribir código limpio, mantenible y entendible para el proceso de desarrollo',
+    
+    'about.title': 'Sobre mi',
+    'about.description': '<p class="text-lightGray text-xl font-light font-opensans lg:text-2xl lg:size-1/2">Hola! Me llamo Ancel, Soy <span class="text-dirtyWhite italic">desarrollador de aplicaciones móviles</span>. Más de <span class="text-dirtyWhite italic">2 años</span> de experiencia.</p>',
+
+    'work.title': 'Experiencia',
+    'work.duration': '4 años 9 meses',
+
+    'projects.title': 'Proyectos',
+
+    'contact.title': 'Contacto',
+    'contact.phone': '626 655 430',
+    'contact.mail': 'anceltests@gmail.com',
+    'contact.cookies': 'Política de cookies'
   },
 } as const;
